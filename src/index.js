b492c863a43ce91eb2066e7ac79f59f391dcbc2d
Anton Yegorov <an_tosha33@mail.ru>
2020-10-18 18:06:27 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import './sass/main.sass'
-console.log('hello world')
-const $ = (className) => {
-  const el = document.querySelector(className);
-  return el;
-};
-
-let  i = 0;
-$('.next').addEventListener('click', () => {
-  const el = $('.slider');
-  el.style.setProperty('--i', ++i)
-})
+import './slider/slider';
